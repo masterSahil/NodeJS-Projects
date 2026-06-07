2ad7d2cc414cd0accf43cbff210183b0b32c0d0e
masterSahil <7882mastersahil@gmail.com>
2026-06-07 11:21:56 +0530

    update

--- 8. Admin Panel Complete/backend/model/user.js
+++ 8. Admin Panel Complete/backend/model/user.js
@@ -1,11 +1,11 @@
 const {Schema, model} = require("mongoose");
 
 const userSchema = new Schema({
-    name: {type: String },
+    name: {type: String},
     email: {type: String, required: true},
-    password: {type: String },
-    phone: {type: Number },
-    role: {type: String },
+    password: {type: String},
+    phone: {type: Number},
+    role: {type: String},
     image: {type: String},
     resetPasswordOtp: {type: String},
     resetPasswordExpires: {type: Date},

a31ef3b68dcbaa62815589395829a34b62afd70a
masterSahil <7882mastersahil@gmail.com>
2026-06-11 17:14:05 +0530

    try to fix

--- 8. Admin Panel Complete/frontend/src/component/Sidebar.jsx
+++ 8. Admin Panel Complete/frontend/src/component/Sidebar.jsx
@@ -175,12 +175,18 @@ export default function Sidebar() {
               </>
             }
 
-            { (role === 'manager' || role === 'superadmin' || role ==='admin') &&
+            { 
               <>
                 {/* Products */}
                 <SidebarDropdown icon={Package} label="Products" activePaths={['/products']} openMenu={openMenu} setOpenMenu={setOpenMenu}>
+                  {
+
+                  }
                   <SidebarItem icon={FolderPlus} label="Add Products" to="/products/add" isNested />
-                  <SidebarItem icon={FolderOpen} label="View Products" to="/products/view" isNested />
+
+                  {(role === 'manager' || role === 'superadmin' || role ==='admin' || role === 'user') &&
+                    <SidebarItem icon={FolderOpen} label="View Products" to="/products/view" isNested />
+                  }
                   <SidebarItem icon={Trash2} label="Trash" to="/products/trash" isNested />
                 </SidebarDropdown>
               </>
